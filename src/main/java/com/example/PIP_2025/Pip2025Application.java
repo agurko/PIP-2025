8f5533cecb0bab6f2a2e50d4f954908a3852873b
Gurko Andrey <agurko@mail.ru>
2025-01-06 15:02:00 +0300

    SpringSecurity-1

---
+++ src/main/java/com/example/PIP_2025/Pip2025Application.java
@@ -0,0 +1,13 @@
+package com.example.PIP_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pip2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pip2025Application.class, args);
+	}
+
+}
